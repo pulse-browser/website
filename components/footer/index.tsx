bb5eb608248c85072379d5687d08a11728af8d65
trickypr <23250792+trickypr@users.noreply.github.com>
2022-01-26 18:23:48 +1100

    💚 Correct ci error

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,4 +1,3 @@
 export const Footer = () => (
-  <footer className="p-8 text-center">&copy; {new Date().getFullYear()} Pulse-Browser</footer>
-  
+  <footer className="p-8 text-center">&copy; {new Date().getFullYear()} Fushra</footer>
 )
