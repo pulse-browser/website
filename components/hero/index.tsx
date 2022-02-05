21bec05be72e8271e82b61945e4a84953c7ba5fc
PressJump <40802367+PressJump@users.noreply.github.com>
2022-02-06 01:16:58 +1100

    💄 Hero Rework #1

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -7,59 +7,66 @@ import {
 import { Button, Nav } from '..'
 
 export const Hero = () => (
-  <div className="bg-gray-100 text-center space-y-8 ">
+  <div className="bg-gray-100">
     <Nav />
+    <div className="grid grid-cols-2 items-center px-40 py-10">
+      <div className="flex justify-center">
+        <div className="text-left w-2/4">
+          <div className="text-7xl inline-flex py-12">
+            <p className="font-bold">Pulse</p>
+            <p className="font-light pl-2">Browser</p>
+          </div>
 
-    <div className="leading-tight">
-      <h1 className="text-7xl font-bold">Pulse Browser</h1>
-      <p className="text-lg font-normal">
-        An experimental Firefox fork that enhances focus and increases work
-        productivity due to its hyper minimalistic UI and built-in tools.
-      </p>
-    </div>
-
-    <div className="flex justify-center break-normal">
-      <div className="w-3/6">
-        <ReactCompareSlider
-          itemOne={
-            <ReactCompareSliderImage
-              src="/assets/dark.png"
-              srcSet="/assets/dark.png"
-              alt="Image one"
-            />
-          }
-          itemTwo={
-            <ReactCompareSliderImage
-              src="/assets/light.png"
-              srcSet="/assets/light.png"
-              alt="Image two"
-            />
-          }
-          handle={
-            <ReactCompareSliderHandle
-              buttonStyle={{
-                backdropFilter: undefined,
-                WebkitBackdropFilter: undefined,
-                boxShadow: undefined,
-                color: 'white',
-                backgroundColor: '#444',
-                border: 0,
-                transform: 'scale(0.5)',
-              }}
-              linesStyle={{ opacity: 0 }}
-            />
-          }
-        />
+          <p className="text-lg font-normal">
+            An experimental Firefox fork that enhances focus and increases work
+            productivity due to its hyper minimalistic UI and built-in tools.
+          </p>
+          <div className="pt-8">
+            <Button href="/download" type="download">
+              <i className="bi bi-download"></i> Download Pulse Browser
+            </Button>
+            <Button href="#features" type="large-text">
+              Learn More
+            </Button>
+          </div>
+        </div>
       </div>
-    </div>
 
-    <div className="">
-      <Button href="/download" type="primary">
-        <i className="bi bi-download"></i> Download
-      </Button>
-      <Button href="#features" type="text">
-        Learn More
-      </Button>
+      <div className="relative z-0">
+        <img src="/assets/pulse-back.svg" className="w-3/4 h-1/4"></img>
+        <div className="w-3/4 absolute inset-0 z-10 flex justify-center items-center">
+          <ReactCompareSlider
+            itemOne={
+              <ReactCompareSliderImage
+                src="/assets/dark.png"
+                srcSet="/assets/dark.png"
+                alt="Image one"
+              />
+            }
+            itemTwo={
+              <ReactCompareSliderImage
+                src="/assets/light.png"
+                srcSet="/assets/light.png"
+                alt="Image two"
+              />
+            }
+            handle={
+              <ReactCompareSliderHandle
+                buttonStyle={{
+                  backdropFilter: undefined,
+                  WebkitBackdropFilter: undefined,
+                  boxShadow: undefined,
+                  color: 'white',
+                  backgroundColor: '#444',
+                  border: 0,
+                  transform: 'scale(0.5)',
+                }}
+                linesStyle={{ opacity: 0 }}
+              />
+            }
+          />
+        </div>
+      </div>
     </div>
 
     <div className="relative">
