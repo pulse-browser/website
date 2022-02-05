8a51ba66e5cbabdb21ebbd1bc65cf7b49aeca072
PressJump <40802367+PressJump@users.noreply.github.com>
2022-02-06 01:01:50 +1100

    💄 Download Button & Large-Text Button

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,7 +1,12 @@
 import Link from 'next/link'
 import { FC } from 'react'
 
-type ButtonType = 'primary' | 'toned-secondary' | 'text'
+type ButtonType =
+  | 'primary'
+  | 'toned-secondary'
+  | 'text'
+  | 'download'
+  | 'large-text'
 
 const InternalButton: FC<{ type: ButtonType; className?: string }> = ({
   children,
@@ -21,6 +26,14 @@ const InternalButton: FC<{ type: ButtonType; className?: string }> = ({
       type == 'text'
         ? 'border-gray-100 text-gray-700 px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-200'
         : ''
+    } ${
+      type == 'download'
+        ? 'bg-blue-500  hover:bg-blue-700 text-white px-6 py-4 shadow-md hover:outline outline-offset-2 outline-blue-500 transition duration-500'
+        : ''
+    } ${
+      type == 'large-text'
+        ? 'border-gray-100 text-gray-700 px-6 py-4 m-2 transition duration-500 ease select-none hover:bg-gray-200'
+        : ''
     } font-bold py-2 px-4 ${className}`}
   >
     {children}
@@ -36,9 +49,7 @@ export const Button: FC<{
     {href ? (
       <Link href={href}>
         <a className={className}>
-          <InternalButton type={type || 'text'}>
-            {children}
-          </InternalButton>
+          <InternalButton type={type || 'text'}>{children}</InternalButton>
         </a>
       </Link>
     ) : (
