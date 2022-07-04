98f019c04d0dd6f9767b483ca2d46727f916252e
trickypr <trickypr@icloud.com>
2022-07-04 16:23:08 +1000

    ✨ Add privacy policy to the website

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,9 +1,16 @@
+import Link from 'next/link'
+
 export const Footer = () => (
   <footer className="p-8 text-center">
-    &copy; {new Date().getFullYear()} TrickyPR, PressJump and contributors under
-    MPL 2.0 -{' '}
-    <a href="https://analytics.fyralabs.com/pulsebrowser.app">
-      View analytics data
-    </a>
+    &copy; {new Date().getFullYear()} TrickyPR, PressJump and contributors under{' '}
+    <Link href={'/legal/license'}>
+      <span className="text-blue-800 underline cursor-pointer">MPL 2.0</span>
+    </Link>{' '}
+    -{' '}
+    <Link href={'/legal/privacy-policy'}>
+      <span className="text-blue-800 underline cursor-pointer">
+        Privacy Policy
+      </span>
+    </Link>
   </footer>
 )
