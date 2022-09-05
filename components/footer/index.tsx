8d90aa9124b10a30b75860630e11122973cca6a2
trickypr <23250792+trickypr@users.noreply.github.com>
2022-09-05 16:59:03 +1000

    🔍️ Use a tag so Google will discover it

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -4,13 +4,13 @@ export const Footer = () => (
   <footer className="p-8 text-center">
     &copy; {new Date().getFullYear()} TrickyPR, PressJump and contributors under{' '}
     <Link href={'/legal/license'}>
-      <span className="text-blue-800 underline cursor-pointer">MPL 2.0</span>
+      <a className="text-blue-800 underline cursor-pointer">MPL 2.0</a>
     </Link>{' '}
     -{' '}
     <Link href={'/legal/privacy-policy'}>
-      <span className="text-blue-800 underline cursor-pointer">
+      <a className="text-blue-800 underline cursor-pointer">
         Privacy Policy
-      </span>
+      </a>
     </Link>
   </footer>
 )
